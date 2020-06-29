58bc634abacab8cbc0022dc25f806e896fdea7c3
felix <felix.cruz@appogeodigital.com>
2020-06-29 17:01:51 +0200

    Proyecto base

---
+++ app/src/main/java/programacionymas/com/AppointmentsActivity.kt
@@ -0,0 +1,31 @@
+package programacionymas.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_appointments.*
+import programacionymas.com.model.Appointment
+
+class AppointmentsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appointments)
+
+        val appointments = ArrayList<Appointment>()
+        appointments.add(
+            Appointment(1, "D1", "12/12/2019", "3:00 PM")
+        )
+        appointments.add(
+            Appointment(2, "D2", "12/12/2019", "4:00 PM")
+        )
+        appointments.add(
+            Appointment(3, "D2", "12/12/2019", "5:00 PM")
+        )
+        appointments.add(
+            Appointment(4, "D4", "12/12/2019", "7:00 PM")
+        )
+
+        rvAppointments.layoutManager = LinearLayoutManager(this)
+        rvAppointments.adapter = AppointmentAdapter(appointments)
+    }
+}
---
+++ app/src/main/java/programacionymas/com/CreateAppointmentActivity.kt
@@ -0,0 +1,117 @@
+package programacionymas.com
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.RadioButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_create_appointment.*
+import java.util.*
+
+class CreateAppointmentActivity : AppCompatActivity() {
+
+    private var selectedCalendar = Calendar.getInstance()
+    private var selectedRadioButton: RadioButton? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_appointment)
+
+        btnNext.setOnClickListener {
+            cvStep1.visibility = View.GONE
+            cvStep2.visibility = View.VISIBLE
+        }
+
+        btnConfirm.setOnClickListener {
+            Toast.makeText(this, "Cita registrada correctamente", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        val Specialtiesoptions = arrayOf("Specialty A", "Specialty B", "Specialty C" )
+        spinnerSpecialties.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Specialtiesoptions)
+
+        val Doctorsoptions = arrayOf("Doctor A", "Doctor B", "Doctor C" )
+        spinnerDocors.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Doctorsoptions)
+    }
+
+    @SuppressLint("StringFormatMatches")
+    fun onclickScheduledDate(v: View?) {
+        val year = selectedCalendar.get(Calendar.YEAR)
+        val month = selectedCalendar.get(Calendar.MONTH)
+        val dayOfMonth = selectedCalendar.get(Calendar.DAY_OF_MONTH)
+
+        //escuchador
+        val listener = DatePickerDialog.OnDateSetListener { datePicker, y, m, d ->
+            //Toast.makeText(this, "$y-$m-$d", Toast.LENGTH_SHORT).show()
+            //mantenemos el dia seleccionado
+            selectedCalendar.set(y, m, d)
+            etScheduledDate.setText(
+                resources.getString(
+                R.string.date_format,
+                y,
+                m.twoDigits(),
+                d.twoDigits()
+                )
+            )
+            //tras seleccionar una fecha llamammos a displayRadioButtons va a mostrar despues de consultar al servidor
+            displayRadioButtons()
+        }
+
+        //new dialog
+        val datePickerDialog = DatePickerDialog(this, listener, year, month, dayOfMonth)
+        val datePicker = datePickerDialog.datePicker
+
+        //establecemos limites
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_MONTH, 1)
+        datePicker.minDate = calendar.timeInMillis // +1
+        calendar.add(Calendar.DAY_OF_MONTH, 29)
+        datePicker.maxDate = calendar.timeInMillis // +30
+
+        // show dialog
+        datePickerDialog.show()
+    }
+
+    private fun displayRadioButtons() {
+        //limpiamos la vista
+  //      radioGroup.clearCheck()
+  //      radioGroup.removeAllViews()
+  //      radioGroup.checkedRadioButtonId
+        selectedRadioButton = null
+        radioGroupLeft.removeAllViews()
+        radioGroupRight.removeAllViews()
+
+        //desmarque de radios
+
+
+        val hours = arrayOf("3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM")
+        var goToLeft = true
+
+        //para cada hora
+        hours.forEach {
+            val radioButton = RadioButton(this)
+            radioButton.id = View.generateViewId()
+            radioButton.text = it
+
+            radioButton.setOnClickListener { view ->
+                selectedRadioButton?.isChecked = false
+                selectedRadioButton = view as RadioButton?
+                selectedRadioButton?.isChecked =true
+            }
+
+            if (goToLeft)
+                radioGroupLeft.addView(radioButton)
+            else
+                radioGroupRight.addView(radioButton)
+            goToLeft = !goToLeft
+        }
+
+    }
+
+    private fun Int.twoDigits() = if (this>=10) this.toString() else "0$this"
+
+}
